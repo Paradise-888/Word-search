c26955ebabfc300947c9283d9df3194488ab1c8c
Paradise-888 <atirach1987thongpakdee@gmail.com>
2025-09-06 01:37:54 +0700

    Add files via upload

--- menu-script.js
+++ menu-script.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (playButton) {
         playButton.addEventListener('click', () => {
-            window.location.href = 'game.html'; // ลิงก์ไปที่หน้าเกม
+            window.location.href = 'game.html';
         });
     }
 
     if (settingButton) {
         settingButton.addEventListener('click', () => {
-            alert('Setting button clicked!'); // สามารถเปลี่ยนเป็นหน้าตั้งค่าได้ในอนาคต
+            alert('Setting button clicked!');
         });
     }
 });
